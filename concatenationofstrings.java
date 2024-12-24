82f4c8bdf56ad8c075ef29f8ab2443b42fe619d2
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2024-12-24 11:23:18 +0530

    Add files via upload

---
+++ concatenationofstrings.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        
+		//concadination//
+		String s1 = "Shalini";
+		String s2 = "Venkatesan";
+		System.out.println(s1 + " " + s2);
+		System.out.println();
+    }
+}
