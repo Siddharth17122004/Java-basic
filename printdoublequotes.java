ed7f9b6f428aed0aa334be376b7db785ff838914
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2024-12-24 11:16:08 +0530

    Update printdoublequotes.java

--- printdoublequotes.java
+++ printdoublequotes.java
@@ -1,13 +1,5 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Success is when your \"signature\" becomes \"autograph\" " );
+        System.out.print("Success is when your \"signature\" becomes \"autograph\" " );
     }
 }
