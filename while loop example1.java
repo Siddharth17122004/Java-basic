608ab96d1068373ade75a8f89381630fb5edd0c3
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2024-12-26 08:54:20 +0530

    Add files via upload

---
+++ while loop example1.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int i=1;
+	    int num=sc.nextInt();
+	    while (i<=num){
+	        if(i%2==0){
+	            System.out.println(i);
+	        }
+	         i++;
+	    }
+	   
+	    
+	 
+	}
+}
