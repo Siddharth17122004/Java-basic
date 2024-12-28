4f2ed37910ef13b5c36b03cc6cb95bae8a9fc740
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2024-12-28 13:38:26 +0530

    Add files via upload

---
+++ Main (7).java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);		
+		int n=sc.nextInt();
+		int sq=n*n;
+		int sum=0;
+		while(sq!=0){
+		    int rem=sq%10;
+		    sum+=rem;
+		    sq=sq/10;
+		}
+		if(n==sum){
+		    System.out.println("Neon");
+		}else{
+		    System.out.print("Not Neon");
+		}
+	}
+}
