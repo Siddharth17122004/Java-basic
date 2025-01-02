1c6e2b3a559935af4c446a04146c2d5900f8552a
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 11:16:27 +0530

    Update understanding variables with examples and erros.java

--- understanding variables with examples and erros.java
+++ understanding variables with examples and erros.java
@@ -1,3 +1,4 @@
+scope of local variable:
 public class Main
 {
     public static double methodA(int a){
@@ -9,3 +10,29 @@ public class Main
 	    
 	}
 }
+output:
+Main.java:8: error: cannot find symbol
+	    System.out.printf("%d", localVar);
+	                            ^
+  symbol:   variable localVar
+  location: class Main
+1 error
+
+Initializing local variable:
+public class Main
+{
+    public static void methodA(int a){
+        
+        int localVar;
+        System.out.println(localVar);
+    }
+	public static void main(String[] args) {
+	    methodA(4);
+	    
+	}
+}
+
+Main.java:6: error: variable localVar might not have been initialized
+        System.out.println(localVar);
+                           ^
+1 error
