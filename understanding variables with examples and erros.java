7e96b6147e4e46d6bcc446b9df64adcd84e9b453
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 11:12:45 +0530

    Add files via upload

---
+++ understanding variables with examples and erros.java
@@ -0,0 +1,11 @@
+public class Main
+{
+    public static double methodA(int a){
+        
+        int localVar=10;
+    }
+	public static void main(String[] args) {
+	    System.out.printf("%d", localVar);
+	    
+	}
+}
