a29f06aee379a5dfdaef4bc599c41f46c6a62412
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 13:44:58 +0530

    Update Instance variable example 2.java

--- Instance variable example 2.java
+++ Instance variable example 2.java
@@ -11,3 +11,27 @@ public class Main
 		System.out.println("obj InstanceVar "+obj2.instanceVar);
 	}
 }
+
+output:
+obj1 instanceVar: 10
+obj2 instanceVar: 0
+obj InstanceVar 20
+
+
+public class Main
+{
+    int instanceVar=5;
+	public static void main(String[] args) {
+	    Main obj1= new Main();
+	    obj1.instanceVar=10;
+	    System.out.println("obj1 instanceVar: "+obj1.instanceVar);
+	    Main obj2=new Main();
+	    System.out.println("obj2 instanceVar: "+obj2.instanceVar);
+		obj2.instanceVar=20;
+		System.out.println("obj InstanceVar "+obj2.instanceVar);
+	}
+}
+output:
+obj1 instanceVar: 10
+obj2 instanceVar: 5
+obj InstanceVar 20
