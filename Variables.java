4061c85667ec2da929280b05959ab2f9473dff48
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 14:22:52 +0530

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -1,3 +1,4 @@
+Trying to access local variable: error in output
 class Main
 {
     int y=222;
@@ -19,3 +20,44 @@ class Main
 	    System.out.println("Main x: "+d.x);
 	}
 }
+output :
+Main.java:10: error: cannot find symbol
+        System.out.println("Mtd-2 x: "+x);
+                                       ^
+  symbol:   variable x
+  location: class Main
+Main.java:19: error: cannot find symbol
+	    System.out.println("Main x: "+d.x);
+	                                   ^
+  symbol:   variable x
+  location: variable d of type Main
+2 errors
+
+Corrected Code:
+	
+class Main
+{
+    int y=222;
+    void method1(){
+        int x=111;
+        System.out.println("Mtd-1 x: "+x);
+        System.out.println("Mtd-1 y: "+y);
+        }
+    void method2(){
+       // System.out.println("Mtd-2 x: "+x);
+        System.out.println("Mtd-2 y: "+y);
+    }
+    
+	public static void main(String[] args) {
+	    Main d = new Main();
+	    d.method1();
+	    d.method2();
+	    System.out.println("Main y: "+d.y);
+	    //System.out.println("Main x: "+d.x);
+	}
+}
+output
+Mtd-1 x: 111
+Mtd-1 y: 222
+Mtd-2 y: 222
+Main y: 222
