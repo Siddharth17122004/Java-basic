90b8f0f34a9375d8a0a3f3c397dd9840f1d0c29a
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 12:00:08 +0530

    Update Instance variable.java

--- Instance variable.java
+++ Instance variable.java
@@ -1,3 +1,4 @@
+ACCESSING:
 public class Main
 {
     int instanceVar=10;
@@ -23,3 +24,32 @@ public class Main
 	    System.out.println("obj2 instanceVar: " +obj2.instanceVar);
 	}
 }
+INITIALIZATION:
+public class Main
+{
+    int intVar;
+    float floatVar;
+    boolean boolVar;
+    String stringVar;
+    
+    public void displayDefaults(){
+       System.out.println("Default int var "+intVar);
+       System.out.println("Default float var "+floatVar);
+       System.out.println("Default boolean var "+boolVar);
+       System.out.println("Default string var "+stringVar);
+    }
+   
+    
+	public static void main(String[] args) {
+	    Main obj1= new Main();
+	    
+	    obj1.displayDefaults();
+	    
+	    
+	}
+}
+output:
+Default int var 0
+Default float var 0.0
+Default boolean var false
+Default string var null
