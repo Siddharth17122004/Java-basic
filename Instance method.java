a165fb1086b7d7b05c5cc683e40427015350d20a
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 14:43:17 +0530

    Update Instance method.java

--- Instance method.java
+++ Instance method.java
@@ -1,3 +1,30 @@
+Accessing without object:
+class Main
+{
+    int instanceVar=42;
+    public void displayInstanceVar(){
+        
+        System.out.println("Instance Variable: "+instanceVar);
+        }
+    public void greet(){
+       
+        System.out.println("Hello from greet method!");
+        displayInstanceVar();
+    }
+    
+	public static void main(String[] args) {
+	    greet();
+	}
+}
+output:
+Main.java:15: error: non-static method greet() cannot be referenced from a static context
+	    greet();
+	    ^
+1 error
+
+	
+	Accessing with object:
+	
 class Main
 {
     int instanceVar=42;
@@ -18,3 +45,7 @@ class Main
 	   
 	}
 }
+output:
+Instance Variable: 42
+Hello from greet method!
+Instance Variable: 42
