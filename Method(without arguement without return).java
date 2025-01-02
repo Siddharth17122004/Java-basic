97aa709a57cda02a01de0d21198e466204be5074
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-02 08:56:22 +0530

    Add files via upload

---
+++ Method(without arguement without return).java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void CalculateSum(){
+        int n=10;
+        int sum=n*(n+1)/2;
+        System.out.println("Sum of numbers from 1 to 10 is: "+sum);
+        }
+    public static void main(String[] args) {
+        CalculateSum();
+    }
+}
