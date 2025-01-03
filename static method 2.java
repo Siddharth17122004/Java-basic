2dbda63273b90e2fe3959df577b9b03e7271a438
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-03 08:47:33 +0530

    Add files via upload

---
+++ static method 2.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    static int staticVar=100;
+    public static void method1(){
+        System.out.println("Inside method1");
+        System.out.println("Static variable "+staticVar);
+        
+    }
+    public static void method2(){
+        System.out.println("Inside method2");
+        method1();//Directly accessing a static variable 
+        Main.method1(); // accessing using class name use this for good programming culture
+    }
+	public static void main(String[] args) {
+	    Main.method2();
+	    
+	}
+}
