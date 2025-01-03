c06a8b52ea19d9cb810e92ec73b923ae4e7d080c
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-03 11:04:39 +0530

    Add files via upload

---
+++ recursion(count digits).java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main
+{   public static int count(int n){
+    if(n==0){
+        return 0;//base case
+    }
+    return 1+count(n/10);//recursive case 
+}
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int res=count(n);
+		System.out.print(res);
+	}
+}
