5ca936318af26eea1a80930227331e003fc36b12
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-07 13:57:19 +0530

    Add files via upload

---
+++ input output array question.java
@@ -0,0 +1,20 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int arr[]={1,2,3,4,5};
+		
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
+		    sum+=arr[i];
+		}
+		System.out.println(sum);
+		for(int i=0;i<arr.length;i++){
+		    arr[i]=sum-arr[i];
+		    sum=arr[i];
+		}
+		for(int i=0;i<arr.length;i++){
+		    System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+}
