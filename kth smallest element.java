546810e5cf289544455402a1af700df353ef8a6d
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-08 09:02:56 +0530

    Add files via upload

---
+++ kth smallest element.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    int arr[]={5,4,1,2,3};
+	    int k=1;
+	    Arrays.sort(arr);
+	    System.out.print(arr[k-1]);
+	   
+	}
+}
