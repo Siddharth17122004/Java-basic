a42d18fd2cdcc0cf543de58d4d9061d08480f91d
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-08 14:46:16 +0530

    Add files via upload

---
+++ rotate array.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main
+{
+    public static void reverse(int arr[],int start,int end){
+        while(start<end){ 
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotate(int arr[],int size,int rot){
+        reverse(arr,0,size-1);
+        reverse(arr,0,rot-1);
+        reverse(arr,rot,size-1);
+    
+    }
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int arr[]={1,2,3,4,5};
+    int rot=sc.nextInt();
+    int size=arr.length;
+    rot=rot%size;
+    rotate(arr,size,rot);
+    
+		for(int i=0;i<arr.length;i++){
+	    System.out.print(arr[i]+" ");
+	}
+}
+}
+	
