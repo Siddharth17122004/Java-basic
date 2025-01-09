77e89998653022043c864dbde299bd2697afbbcf
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-09 08:49:03 +0530

    Add files via upload

---
+++ partial right rotation.java
@@ -0,0 +1,34 @@
+import java.util.*;
+public class Main
+{
+    public static void reverse(int arr[],int start,int end){
+        while(start<end){ 
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+    }
+    public static void rotate(int arr[],int fhs,int rot){
+        reverse(arr,0,fhs-1);
+        reverse(arr,0,rot-1);
+        reverse(arr,rot,fhs-1);
+    
+    }
+	public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int arr[]={1,2,3,4,5};
+    int rot=sc.nextInt();
+    int size=arr.length;
+    int mid=size/2;
+    int fhs=size-mid;
+    rot=rot%size;
+    rotate(arr,fhs,rot);
+    
+		for(int i=0;i<arr.length;i++){
+	    System.out.print(arr[i]+" ");
+	}
+}
+}
+	
