6700205776be5eb7960e113746057e2cbad26792
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-09 09:37:24 +0530

    Update left rotation.java

--- left rotation.java
+++ left rotation.java
@@ -11,7 +11,8 @@ public class Main
         }
     }
     public static void rotate(int arr[],int size,int rot){
-        reverse(arr,0,rot-1);
+        rot=rot%size;
+	reverse(arr,0,rot-1);
         reverse(arr,rot,size-1);
         reverse(arr,0,size-1);
     
@@ -21,7 +22,7 @@ public class Main
 	int arr[]={1,2,3,4,5};
     int rot=sc.nextInt();
     int size=arr.length;
-    rot=rot%size;
+    
     rotate(arr,size,rot);
     
 		for(int i=0;i<arr.length;i++){
