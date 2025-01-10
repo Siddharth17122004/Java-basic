2e65d2f247f04fca3449064147021549b97d2c3b
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-10 14:14:35 +0530

    Update Number of lower,upper case ,digits(without method).java

--- Number of lower,upper case ,digits(without method).java
+++ Number of lower,upper case ,digits(without method).java
@@ -2,7 +2,7 @@ public class Main
 {
 	public static void main(String[] args) {
 		String str=" 3RoseS ";
-		str=str.trim();
+		str=str.replaceAll(" ","");
 	    int u=0,l=0,d=0,s=0;
 		for(int i=0;i<str.length();i++){
 		    char ch=str.charAt(i);  
