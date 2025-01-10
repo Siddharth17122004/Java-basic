140e633a3040413783770f0022f53ff299aa4a20
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-10 14:07:38 +0530

    Update Number of lower,upper case ,digits(without method).java

--- Number of lower,upper case ,digits(without method).java
+++ Number of lower,upper case ,digits(without method).java
@@ -1,8 +1,9 @@
 public class Main
 {
 	public static void main(String[] args) {
-		String str="3RoseS";
-	    int u=0,l=0,d=0;
+		String str=" 3RoseS ";
+		str=str.trim();
+	    int u=0,l=0,d=0,s=0;
 		for(int i=0;i<str.length();i++){
 		    char ch=str.charAt(i);  
 		    if(ch>='A'&&ch<='Z'){
@@ -11,10 +12,13 @@ public class Main
 		        l++;
 		    }else if(ch>='0'&&ch<='9'){
 		        d++;
-		    }
+		    }else{
+			    s++;
+		}
 		}
 		System.out.println("uppercase: "+u);
 		System.out.println("lowercase: "+l);
 		System.out.println("digits: "+d);
+		System.out.print("Special characters: "+s);
 	}
 }
