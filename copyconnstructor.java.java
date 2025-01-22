f6720dce0ff326815a744100e7368fa304a87369
VENKATASUBRAMANIAM R <piroplayer1920@gmail.com>
2025-01-22 13:21:22 +0530

    Add files via upload

---
+++ copyconnstructor.java.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Sample
+{
+    int x,y;
+    public Sample(){
+        x=10;
+        y=20;
+    }
+    public Sample(Sample s1){
+        x=s1.x;
+        y=s1.y;
+    }
+    public void display(){
+        System.out.println(x);
+        System.out.println(y);
+    }
+	public static void main(String[] args) {
+		Sample s1=new Sample();
+		Sample s2=new Sample(s1);
+		System.out.println("Data from first constructor");
+		s1.display();
+		System.out.println("data from second constructor");
+		s2.display();
+	}
+}
